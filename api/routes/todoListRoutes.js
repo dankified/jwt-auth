635a48edf22e3f30e4e3bf190757f266eb29a356
Patricio de la Guardia <padlgt01@gmail.com>
2018-11-21 10:55:24 -0500

    Finish authentication process

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,13 +1,20 @@
 module.exports = function(app) {
 	let todos = require('../controllers/todoListController');
+	let userHandlers = require('../controllers/userController');
 
 	// todoList Routes
 	app.route('/tasks')
 			.get(todos.listAllTodos)
-			.post(todos.createTodo);
+			.post(userHandlers.loginRequired, todos.createTodo);
 	
 	app.route('/tasks/:taskId')
 		.get(todos.readTodo)
 		.put(todos.updateTodo)
 		.delete(todos.deleteTodo);
+
+	app.route('/auth/register')
+		.post(userHandlers.register);
+
+	app.route('/auth/sign_in')
+		.post(userHandlers.signIn);
 };
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const Todo = require('./api/models/todoModel');
+const User = require('./api/models/userModel');
 const todoRoutes = require('./api/routes/todoListRoutes');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
@@ -13,6 +15,18 @@ mongoose.connect('mongodb://localhost:27017/Practice', {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+	if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+		jwt.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+			if(err) req.user = undefined;
+			req.user = decode;
+		});
+	} else {
+		req.user = undefined;
+		next();
+	}
+});
 
 todoRoutes(app);
 
